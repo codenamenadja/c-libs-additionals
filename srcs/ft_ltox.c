a99ef030d083d1c8dd1688c94e6a51b67ec6da54
junehan <junehan.dev@gmail.com>
2020-07-23 13:10:33 +0900

    spec chage,itox to ltox

--- srcs/ft_ltox.c
+++ srcs/ft_ltox.c
@@ -6,35 +6,37 @@
 /*   By: jihhan <junehan.dev@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 08:09:58 by jihhan            #+#    #+#             */
-/*   Updated: 2020/07/19 21:06:32 by jihhan           ###   ########.fr       */
+/*   Updated: 2020/07/23 13:09:07 by jihhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-char        *ft_itox(int n)
+char        *ft_ltox(long n)
 {
     char            i;
-    char            buf[16];
+    char            buf[32];
     char            *buf_pt;
-    unsigned int    val;
-    unsigned int    msk;
+    unsigned long   data;
+    unsigned long   val;
+    unsigned long   msk;
 
     if (!n)
         return strdup("0\0");
-    n = (unsigned int) n;
-    msk = 0b11110000000000000000000000000000;
-    buf_pt = buf;
+    data = (unsigned long) n; 
+    msk = 15;
+    *(buf + 31) = 0;
+    buf_pt = (buf + 30);
     i = -1;
-    while (++i < 8)
+    val = 1;
+    while (++i < 16 && data)
     {
-        if ((val = (n & msk)))
-            val >>= (4 * (7-i));
-        if (val || buf_pt != buf)
-            *buf_pt++ = *("0123456789abcdef" + val);
-        msk >>= 4;
+        val = (data & msk);
+        data -= val;
+        val >>= (4 * i);
+        *buf_pt-- = *("0123456789abcdef" + val);
+        msk <<= 4;
     }
-    *buf_pt = '\0';
-    return strdup(buf);
+    return strdup(buf_pt+1);
 }
 
